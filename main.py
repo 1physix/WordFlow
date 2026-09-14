fea2928798688623305a68d874aa2157425b800e
Ishaq <Ishaq.Hussain@icloud.com>
2026-09-14 13:06:32 +0100

    No way this actually reads and audio file and transcribes itgit add main.py

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 import sounddevice as sd
 import mlx_whisper as mlx
-"""
-#note to self - for play/rec, you need to have a wait after each one in order to record the correct amount of audio.
-duration = 5 #secs
-fs = 48000 #Hz
-sd.default.channels = 1
-myrecording = sd.rec(int(fs * duration), samplerate=fs)
-sd.wait()
-sd.play(myrecording, samplerate=fs)
-sd.wait()
-print(myrecording.shape)
-print(fs * duration)
-for i in range(0,192001,48000):
-    print(myrecording[i:i+48000])
-"""
+from mlx_whisper.load_models import load_model
+from scipy.signal import resample
 
 model = "mlx-community/whisper-turbo"
-
-text1 = mlx.transcribe("TestAudio1.m4a", path_or_hf_repo = model)['text']
-print(text1)
-
-
-text2 = mlx.transcribe("TestAudio2.m4a", path_or_hf_repo = model)['text']
-print(text2)
+loaded_model = load_model(model)
+sample_rate = 16000 #Whisper only accepts audio files with a sample rate of 16kHz
 
 """
 Models tested (replace to use): 
@@ -30,3 +13,23 @@ Models tested (replace to use):
 > mlx-community/whisper-large-v3-turbo #Accurate, a bit quicker than the prev model, but still a bit slow.
 > mlx-community/whisper-turbo #Accurate, the fastest of the three, but not by that much.
 """
+
+
+
+#note to self - for play/rec, you need to have a wait after each one in order to record the correct amount of audio.
+duration = 5 #secs
+fs = 48000 #Hz
+sd.default.channels = 1
+
+myrecording = sd.rec(int(fs * duration), samplerate=fs)
+sd.wait()
+myrecording_flat = myrecording.flatten()
+
+sd.play(myrecording_flat, samplerate=fs)
+sd.wait()
+
+myrecording_flat_resampled = resample(myrecording_flat, sample_rate*duration)
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.resample.html
+
+text2 = mlx.transcribe(myrecording_flat_resampled, path_or_hf_repo = model)["text"]
+print(text2)
